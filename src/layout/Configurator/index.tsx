c9888c19f5e35e6965d2532a165745a0018eba70
crypto-zombie <valcano103@gmail.com>
2023-12-12 18:26:46 +0200

    feat: add scrolltriger

--- src/layout/Configurator/index.tsx
+++ src/layout/Configurator/index.tsx
@@ -7,12 +7,22 @@ import Loading from 'components/Loading';
 export default function Configurator() {
   return (
     <div className={Style.wrapper}>
-      <Canvas camera={{ position: [0, 20, 40], fov: 50 }} dpr={[1, 2]}>
-        <Scene />
-      </Canvas>
+      <div className='fixed h-screen w-screen'>
+        <Canvas camera={{ position: [0, 20, 40], fov: 50 }} dpr={[1, 2]}>
+          <Scene />
+        </Canvas>
+      </div>
       <p className='absolute bottom-12 w-full animate-bounce text-center text-xs text-amber-100 drop-shadow-md'>
         SCROLL DOWN TO EXPLOR
       </p>
+
+      <div className='viewport'>
+        <div className='child1 h-screen'></div>
+        <div className='child2 h-screen'></div>
+        <div className='child3 h-screen'></div>
+        <div className='child4 h-screen'></div>
+      </div>
+
       <Loading />
     </div>
   );
--- src/layout/Scene/index.tsx
+++ src/layout/Scene/index.tsx
@@ -1,10 +1,42 @@
 /* eslint-disable react/no-unknown-property */
 import { Environment } from '@react-three/drei';
+import { useThree } from '@react-three/fiber';
 import { Bloom, EffectComposer } from '@react-three/postprocessing';
 import Model from 'components/Model';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/all';
 import { Suspense } from 'react';
 
+gsap.registerPlugin(ScrollTrigger);
 export default function Scene() {
+  const { camera } = useThree();
+
+  const tl = gsap.timeline();
+  tl.to(camera.rotation, {
+    y: 0.1,
+    ease: 'power2.inOut',
+    overwrite: true,
+    scrollTrigger: { trigger: '.child1', scrub: 1, pin: true },
+  })
+    .to(camera.rotation, {
+      y: 0.3,
+      overwrite: true,
+      ease: 'power2.inOut',
+      scrollTrigger: { trigger: '.child2', scrub: 1, pin: true },
+    })
+    .to(camera.rotation, {
+      y: 0.3,
+      overwrite: true,
+      ease: 'power2.inOut',
+      scrollTrigger: { trigger: '.child3', scrub: 1, pin: true },
+    })
+    .to(camera.rotation, {
+      y: 0.3,
+      overwrite: true,
+      ease: 'power2.inOut',
+      scrollTrigger: { trigger: '.child4', scrub: 1, pin: true },
+    });
+
   return (
     <Suspense fallback={null}>
       <Model />
