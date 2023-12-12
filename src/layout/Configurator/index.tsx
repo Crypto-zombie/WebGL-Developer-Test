fed687ffdbeb2657880067b45b5678bbb7cc94d7
crypto-zombie <valcano103@gmail.com>
2023-12-12 17:07:44 +0200

    chore: update version

--- src/layout/Configurator/index.tsx
+++ src/layout/Configurator/index.tsx
@@ -2,12 +2,15 @@
 import { Canvas } from '@react-three/fiber';
 import Style from './style.module.scss';
 import Scene from 'layout/Scene';
+import { Environment, OrbitControls } from '@react-three/drei';
 
 export default function Configurator() {
   return (
     <div className={Style.wrapper}>
       <Canvas camera={{ position: [0, 0, 80], fov: 50 }} dpr={[1, 2]}>
         <Scene />
+        <OrbitControls />
+        <Environment preset='city' />
       </Canvas>
     </div>
   );
--- src/layout/Scene/index.tsx
+++ src/layout/Scene/index.tsx
@@ -1,3 +1,11 @@
+/* eslint-disable react/no-unknown-property */
+import Model from 'components/Model';
+
 export default function Scene() {
-  return <ambientLight />;
+  return (
+    <>
+      <Model />
+      <ambientLight intensity={1} />
+    </>
+  );
 }
